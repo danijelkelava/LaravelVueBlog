e16f352df3eee4df871bf68bfe1a522334dfd2cc
danijelkelava <dykyprod@gmail.com>
2018-04-07 16:33:20 +0200

    error page 404

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+    	return view('index');
+    }
+
+    public function show()
+    {
+    	return view('blog');
+    }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,4 @@
+
+
+
+<h2>404 This route doesn't exist</h2>
