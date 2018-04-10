c97d6f918a272efc84ab3bf7bf8a8d06fab21c4b
danijelkelava <dykyprod@gmail.com>
2018-04-11 00:31:38 +0200

    testing i dalje

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,20 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Faker\Factory as Faker;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $faker;
+	
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+    }
 }
--- tests/Unit/CreatePostTest.php
+++ tests/Unit/CreatePostTest.php
@@ -4,28 +4,11 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Faker\Factory as Faker;
+
 
 class ExampleTest extends TestCase
 {
-	protected $faker;
-    /**
-     * Set up the test
-     */
-    public function setUp()
-    {
-        parent::setUp();
-        $this->faker = Faker::create();
-    }
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
-    }
+
 
     /** @test */
     public function store_post()
